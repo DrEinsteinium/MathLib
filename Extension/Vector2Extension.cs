52b0a71694cc9d68905bb4c085cf529291b06715
Tyler Morton <tmmorton@fullsail.edu>
2016-12-11 16:48:25 -0500

    Fixed the namespacing of Vector2Extension and added comments to the functions.

--- Extension/Vector2Extension.cs
+++ Extension/Vector2Extension.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class Vector2Extension
-{
-    public static void FlipX(this Vector2 vec)
-    {
-        vec.x = vec.x * -1;
-    }
 
-    public static void FlipY(this Vector2 vec)
+namespace MathLib.Extensions
+{
+    public static class Vector2Extension
     {
-        vec.y = vec.y * -1;
+        /// <summary>
+        /// Multiplies the X component of this vector by -1
+        /// </summary>
+        public static void FlipX(this Vector2 vec)
+        {
+            vec.x = vec.x * -1;
+        }
+
+        /// <summary>
+        /// Multiplies the Y component of this vector by -1
+        /// </summary>
+        public static void FlipY(this Vector2 vec)
+        {
+            vec.y = vec.y * -1;
+        }
     }
 }
