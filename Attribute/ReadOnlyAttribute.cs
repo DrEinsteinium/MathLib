6c59fcb6c27c7d283f569e61d960bcd1869a5add
Tyler Morton <tmmorton@fullsail.edu>
2016-12-11 16:38:58 -0500

    Added a ReadOnly attribute and PropertyDrawer to allow for items to be visible in the inspector but not editable.

---
+++ Attribute/ReadOnlyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MathLib.Attribute
+{
+    /// <summary>
+    /// Attribute that allows the programmer to display a field in the inspector
+    /// but does not allow editing of that field. Used for things like UUIDs
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {   }
+}
---
+++ Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using MathLib.Attribute;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
